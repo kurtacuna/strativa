c71126fc8f46e14c87ac73d8cced18ed9dc08dbf
TheBasicProgrammer <ryfielvgonzales@gmail.com>
2025-05-04 22:57:11 +0800

    Finalized transfer screen connection

--- strativa_frontend/lib/src/entrypoint/controllers/bottom_nav_notifier.dart
+++ strativa_frontend/lib/src/entrypoint/controllers/bottom_nav_notifier.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class BottomNavNotifier with ChangeNotifier {
-  int _index = 1;
+  int _index = 0;
   get getIndex => _index;
   set setIndex(int index) {
     _index = index;
