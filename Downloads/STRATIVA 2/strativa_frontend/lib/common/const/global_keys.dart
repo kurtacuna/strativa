02292d89492d8aa7ba34ff40f8d6c0118490512d
TheBasicProgrammer <ryfielvgonzales@gmail.com>
2025-05-04 23:02:59 +0800

    minor changes transfer

--- Downloads/STRATIVA 2/strativa_frontend/lib/common/const/global_keys.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppGlobalKeys {
-  static final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
-  static final GlobalKey<FormState> loginFormKey = GlobalKey<FormState>();
-  static final GlobalKey<FormState> generateQrFormKey = GlobalKey<FormState>();
-}
